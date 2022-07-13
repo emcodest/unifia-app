75d8b7f2c1601a2ed41c905549e9ec0383623857
emcodest <eeema9@gmail.com>
2022-07-13 08:51:23 +0100

    update

---
+++ ff.js
@@ -0,0 +1,16 @@
+{
+    response_body: [
+        {
+
+            "data_key": "phone", "data_name": "phone", "data_type": "TEXT_INPUT", "data_value": ""
+
+        },
+        {
+
+            "data_key": "email_address", "data_name": "Enter your email address", "data_type": "TEXT_INPUT"
+
+
+        },
+
+    ]
+}
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Unifia ThirdParty App' });
+  res.render('index', { title: 'Unifia Sample Template ThirdParty App' });
 });
 
 /**
@@ -13,50 +13,91 @@ router.get('/', function (req, res, next) {
 //: Example:Collect user phone number and email address to send airtime to phone
 //: step one
 router.post('/remote-input-1', function (req, res, next) {
-  const model = [
-    {
-     "phone": ""
-    },
-    {
-     "email": ""
-    }
+  const model = {
+    step: 1,
+    type: 'input',
+    status: 'success or error',
+    message: 'Completed',
+    response_body: [
+      {
 
-]
+        "data_key": "phone", "data_name": "phone", "data_type": "TEXT_INPUT", "data_value": ""
+
+      },
+      {
+
+        "data_key": "email_address", "data_name": "Enter your email address", "data_type": "TEXT_INPUT"
+
+
+      },
+
+    ]
+  }
   res.json(model)
 });
 //: step two
 //: confirm the phone number and email the user selected
 router.post('/remote-input-2', function (req, res, next) {
-  const model = [
-    {
-     "phone": "xxx"
-    },
-    {
-     "email": "xxx"
-    }
+  const model = {
+    step: 2,
+    type: 'input',
+    status: 'success or error',
+    message: 'Completed',
+    response_body: [
+      {
 
-]
+        "data_key": "phone", "data_name": "phone", "data_type": "TEXT_BOX", "data_value": "080-----2222"
+
+      },
+      {
+
+        "data_key": "email_address", "data_name": "Enter your email address", "data_type": "TEXT_BOX", "data_value": "xxxx@gmail.com"
+
+
+      },
+
+    ]
+  }
+
   res.json(model)
 });
 /**
  *  INVOICE ENDPOINT
  */
 
-router.post('/invoice', function (req, res, next) {
-  const {} = req.body
+router.post('/remote-invoice', function (req, res, next) {
+//: TODO:
+  //: BUSINESS LOGIC
+  //: TODO: CONFIRM invoice
+ // Verify Invoice Endpoint - Invoice Confirmation GET request
+//https://unifia.herokuapp.com/invoice-confirm?no=1234
   const model = {
-    "amount": "100",
-    "message": "Please pay"
+    step: 3,
+    type: 'invoice',
+    status: 'success or error',
+    message: 'Completed',
+    amount: '100'
   }
+
   res.json(model)
 });
 /**
  *  DELIVERY ENDPOINT
  */
 
-router.post('/delivery', function (req, res, next) {
+router.post('/remote-delivery', function (req, res, next) {
+
+  //: TODO:
+  //: BUSINESS LOGIC
+  //: TODO: CONFIRM PURCHASE BEFORE GIVING VALUE
+  // Service Purchase Confirmation GET request
+//https://unifia.herokuapp.com/confirm-purchase?invoice_no=1234
+
   const model = {
-    message: "Thanks for your payment"
+    step: 4,
+    type: 'delivery',
+    status: 'success or error',
+    message: 'Delivered successfully'
   }
   res.json(model)
 });
